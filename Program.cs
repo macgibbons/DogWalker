46df35f62b3a46f1fd1dc6c1dee59adb4f9b043e
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-03-24 10:42:30 -0500

    forgot to save the changes in program.cs last commit

--- Program.cs
+++ Program.cs
@@ -9,63 +9,63 @@ namespace DogWalker
     {
         static void Main(string[] args)
         {
-            //WalkerRepository walkerRepo = new WalkerRepository();
-            //NeighborhoodRepository neighborhoodRepo = new NeighborhoodRepository();
+            WalkerRepository walkerRepo = new WalkerRepository();
+            NeighborhoodRepository neighborhoodRepo = new NeighborhoodRepository();
             OwnerRepository ownerRepo = new OwnerRepository();
 
-            //Console.WriteLine("Getting All Walkers:");
-            //Console.WriteLine();
+            Console.WriteLine("Getting All Walkers:");
+            Console.WriteLine();
 
-            //List<Walker> allWalkers = walkerRepo.GetAllWalkers();
+            List<Walker> allWalkers = walkerRepo.GetAllWalkers();
 
 
-            //foreach (Walker walker in allWalkers)
-            //{
-            //    Console.WriteLine($"{walker.Id}.) {walker.Name} Walks dogs in  {walker.Neighborhood.Name}.");
-            //}
+            foreach (Walker walker in allWalkers)
+            {
+                Console.WriteLine($"{walker.Id}.) {walker.Name} Walks dogs in  {walker.Neighborhood.Name}.");
+            }
 
-            //Console.WriteLine("--------------------");
-            //Console.WriteLine("Show Walkers in specific neighborhood");
-            //Console.WriteLine();
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Show Walkers in specific neighborhood");
+            Console.WriteLine();
 
-            //List<Neighborhood> allNeighborhoods = neighborhoodRepo.GetAllNeighborhoods();
-            //foreach (var n in allNeighborhoods)
-            //{
-            //    Console.WriteLine($"{n.Id} {n.Name}");
-            //}
+            List<Neighborhood> allNeighborhoods = neighborhoodRepo.GetAllNeighborhoods();
+            foreach (var n in allNeighborhoods)
+            {
+                Console.WriteLine($"{n.Id} {n.Name}");
+            }
 
-            //var userInput = int.Parse(Console.ReadLine());
-            //Walker singleWalker = walkerRepo.GetWalkerByNeighborhood(userInput);
+            var userInput = int.Parse(Console.ReadLine());
+            Walker singleWalker = walkerRepo.GetWalkerByNeighborhood(userInput);
 
-            //Console.WriteLine($"---- Dog walkers in {singleWalker.Neighborhood.Name} ----");
-            //Console.WriteLine($"{singleWalker.Id}.) {singleWalker.Name} ");
+            Console.WriteLine($"---- Dog walkers in {singleWalker.Neighborhood.Name} ----");
+            Console.WriteLine($"{singleWalker.Id}.) {singleWalker.Name} ");
 
-            //Console.ReadLine();
-            //Console.Clear();
+            Console.ReadLine();
+            Console.Clear();
 
-            //Console.WriteLine("---- Add a new Walker ----");
-            //Console.WriteLine();
-            //Console.WriteLine("What is their name?");
-            //var NewWalkerName = Console.ReadLine();
-            //Console.WriteLine($"What neighborhood does {NewWalkerName} work in?");
-            //foreach (var n in allNeighborhoods)
-            //{
-            //    Console.WriteLine($"{n.Id} {n.Name}");
-            //}
+            Console.WriteLine("---- Add a new Walker ----");
+            Console.WriteLine();
+            Console.WriteLine("What is their name?");
+            var NewWalkerName = Console.ReadLine();
+            Console.WriteLine($"What neighborhood does {NewWalkerName} work in?");
+            foreach (var n in allNeighborhoods)
+            {
+                Console.WriteLine($"{n.Id} {n.Name}");
+            }
 
-            //var NewWalkerNeighborhoodId = int.Parse(Console.ReadLine());
-            //Walker NewWalker = new Walker
-            //{
-            //    Name = NewWalkerName,
-            //    NeighborhoodId = NewWalkerNeighborhoodId
-            //};
+            var NewWalkerNeighborhoodId = int.Parse(Console.ReadLine());
+            Walker NewWalker = new Walker
+            {
+                Name = NewWalkerName,
+                NeighborhoodId = NewWalkerNeighborhoodId
+            };
 
-            //walkerRepo.AddWalker(NewWalker);
+            walkerRepo.AddWalker(NewWalker);
 
-            //Console.WriteLine($"{NewWalker.Name} has been added!");
+            Console.WriteLine($"{NewWalker.Name} has been added!");
 
-            //Console.ReadLine();
-            //Console.Clear();
+            Console.ReadLine();
+            Console.Clear();
 
             Console.WriteLine("----Showing all Owners----");
             List<OWNER> allOwners =ownerRepo.GetAlOwners();
